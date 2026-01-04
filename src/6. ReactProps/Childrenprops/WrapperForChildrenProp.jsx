c2bdcb0eae2dfdaf580d9cf6d671f2fdd5c383c4
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-04 20:42:46 +0530

    Created wrapper for children cards of childrenProps.

---
+++ src/6. ReactProps/Childrenprops/WrapperForChildrenProp.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function WrapperForChildrenProp({ children }) {
+  return <div className="wrapper-childrencard">{children}</div>;
+}
+
+export default WrapperForChildrenProp;
