dc531fe1204211842eab95aa3c30f34cab864c8f
rinkal159 <rinkalsingapuri@gmail.com>
2026-01-04 20:39:01 +0530

    Heading of parts of basic props section.

---
+++ src/6. ReactProps/BasicProps/BasciPropsSubPartsHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BasciPropsSubPartsHeading({partHeading}) {
+  return (
+    <h2 className="part-heading">{partHeading}</h2>
+  )
+}
+
+export default BasciPropsSubPartsHeading
